60af25ac12262e665ccfb78ea1b36d745bba1ebe
AdhamARabie <127603170+AdhamARabie@users.noreply.github.com>
2023-03-21 18:32:19 +0200

    Database

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 
 /*
 |--------------------------------------------------------------------------
@@ -34,4 +35,9 @@ Route::post('/about', function () {
 Route::get('/tasks', function () {
     $tasks  = ['Task 1', 'Task 2', 'Task 3'];
     return view('Tasks', compact('tasks'));
+});
+
+Route::get('/tasks-db', function () {
+    $tasks  = DB::table('tasks')->get();
+    return view('TasksDB', compact('tasks'));
 });
