95c00d751a2e34396230013031de38ecbb8f0b25
Dark084 <79041023+Dark084@users.noreply.github.com>
2023-03-27 16:36:09 +0200

    add and delete from db, not ready

--- routes/web.php
+++ routes/web.php
@@ -46,3 +46,19 @@ Route::get('/tasklist', function () {
     $tasks  = DB::table('tasks')->get();
     return view('TaskList', compact('tasks'));
 });
+
+Route::post('/add', function () {
+    DB::table('tasks')->insert([
+        'id' => 4,
+        'name' => $_POST['name'],
+        'created_at' => now(),
+        'updated_at' => now()
+    ]);
+
+    return redirect()->back();
+});
+
+Route::post('/delete', function () {
+    DB::table('tasks')->where('id', $_POST['id'])->delete();
+    return redirect()->back();
+});
