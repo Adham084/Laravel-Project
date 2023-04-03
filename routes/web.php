9489739b59591bdaa233b4e9020c10981d18e0b6
Adham084 <79041023+Adham084@users.noreply.github.com>
2023-04-03 12:50:11 +0200

    some cleaning

--- routes/web.php
+++ routes/web.php
@@ -14,34 +14,6 @@ use Illuminate\Support\Facades\DB;
 |
 */
 
-Route::get('/', function () {
-    return view('Welcome');
-});
-
-Route::get('/hello', function () {
-    return view('Hello');
-});
-
-Route::get('/about', function () {
-    $name  = 'Asd';
-    return view('About', compact('name'));
-});
-
-Route::post('/about', function () {
-    $name  = $_POST['name'];
-    return view('About', compact('name'));
-});
-
-Route::get('/tasks', function () {
-    $tasks  = ['Task 1', 'Task 2', 'Task 3'];
-    return view('Tasks', compact('tasks'));
-});
-
-Route::get('/tasks-db', function () {
-    $tasks  = DB::table('tasks')->get();
-    return view('TasksDB', compact('tasks'));
-});
-
 Route::get('/tasklist', function () {
     $tasks  = DB::table('tasks')->get();
     return view('TaskList', compact('tasks'));
